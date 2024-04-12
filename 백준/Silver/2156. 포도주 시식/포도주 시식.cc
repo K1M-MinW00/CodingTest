689074c195da96ff42ccd6d224d5e870a5ff7ad9
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2024-04-12 15:21:50 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	vector<int> v(n + 1, 0);
+	vector<int> dp(n + 1, 0);
+
+	for (int i = 1; i <= n; ++i)
+		cin >> v[i];
+
+
+	dp[1] = v[1];
+
+	if (n == 1)
+	{
+		cout << v[1];
+		return 0;
+	}
+
+	dp[2] = v[1] + v[2];
+
+	for (int i = 3; i <= n; ++i)
+		dp[i] = max(dp[i - 1], max(dp[i - 2], dp[i - 3] + v[i - 1]) + v[i]);
+
+	cout << dp[n];
+
+	return 0;
+}
