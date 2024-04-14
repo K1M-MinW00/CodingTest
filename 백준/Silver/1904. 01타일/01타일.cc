80fd2ff7668decb5d8d4aa3426698fffe6c4faa0
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2024-04-15 03:34:39 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> v(n + 1, 0);
+
+	v[1] = 1;
+	v[2] = 2;
+
+	for (int i = 3; i <= n; ++i)
+		v[i] = (v[i - 1] + v[i - 2]) % 15746;
+
+	cout << v[n];
+
+	return 0;
+}
