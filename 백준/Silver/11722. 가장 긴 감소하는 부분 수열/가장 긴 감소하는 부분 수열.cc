f8423212cf69f863d032cabc85ca64a515c329fd
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2024-11-15 22:00:52 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n + 1);
+	vector<int> dp(n + 1);
+
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> v[i];
+		dp[i] = 1;
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= i; ++j)
+		{
+			if (v[i] < v[j] && dp[j] + 1 > dp[i])
+				dp[i] = dp[j] + 1;
+		}
+	}
+
+	int max = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (dp[i] > max)
+			max = dp[i];
+	}
+
+	cout << max;
+	return 0;
+}
