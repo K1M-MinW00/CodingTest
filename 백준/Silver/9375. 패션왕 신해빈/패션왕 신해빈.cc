f58b716f4bd8b02003b0dc9a6e02ceb3fe2653ed
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2024-12-10 19:58:41 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -8,46 +8,36 @@ using namespace std;
 int main()
 {
 	int n, k;
-
+	string name,type;
 	cin >> n;
 
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> k;
-
-		map<string, vector<string>> m;
+		map < string,vector<string>> m;
 
 		for (int j = 0; j < k; ++j)
 		{
-			string name, type;
 			cin >> name >> type;
 
 			if (m.find(type) != m.end())
 			{
 				m[type].push_back(name);
 			}
-
 			else
 			{
-				vector<string> temp = { name };
-				m.insert(make_pair(type, temp));
+				vector<string> temp;
+				temp.push_back(name);
+				m[type] = temp;
 			}
 		}
 
-		vector<int> answer;
-		int result = 1;
+		int answer = 1;
 
-		for (auto it = m.begin(); it != m.end(); it++)
-		{
-			int cnt = it->second.size();
-			answer.push_back(cnt);
-		}
+		for (auto e : m)
+			answer *= (e.second.size()+1);
 
-		for (int j = 0; j < answer.size(); ++j)
-		{
-			result *= (answer[j] + 1);
-		}
-		cout << result - 1 << '\n';
+		cout << answer - 1 << '\n';
 	}
 
 	return 0;
