c03b9035cda263d5525d4191e52e97df3b868c48
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2024-12-14 21:32:28 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> arr(10);
+
+
+void dfs(int num, int k)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; ++i)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = num; i <= n; ++i)
+	{
+		arr[k] = i;
+		dfs(i + 1, k + 1);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+
+	dfs(1, 0);
+	return 0;
+}
