97daeb11cdc53c36fa03f6e41bedd10ff914abd2
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-01-06 00:24:25 +0900

    [Silver II] Title: N과 M (12), Time: 4 ms, Memory: 2624 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+vector<int> a;
+vector<int> visited;
+
+vector<vector<int>> answer;
+
+void solve(int idx, vector<int> v)
+{
+	if (v.size() == m)
+	{
+		answer.push_back(v);
+		return;
+	}
+
+	for (int i = idx; i < n; ++i)
+	{
+
+		v.push_back(a[i]);
+		solve(i, v);
+		v.pop_back();
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+
+	a.resize(n);
+	visited.resize(n, false);
+
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+
+	sort(a.begin(), a.end());
+
+	vector<int> temp;
+
+	for (int i = 0; i < n; ++i)
+	{
+		temp.push_back(a[i]);
+		solve(i, temp);
+		temp.pop_back();
+	}
+
+	sort(answer.begin(), answer.end());
+	temp.clear();
+
+	for (auto v : answer)
+	{
+		if (temp == v)
+			continue;
+		for (int i = 0; i < m; ++i)
+			cout << v[i] << ' ';
+		cout << '\n';
+
+		temp = v;
+	}
+
+	return 0;
+}
