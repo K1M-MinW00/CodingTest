a34a49c1b82df11494d7cfdf482a0773a5ddc0cf
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-04-01 23:51:49 +0900

    [Silver II] Title: K번째 소수, Time: 4 ms, Memory: 2996 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/15965. K번째 소수/K번째 소수.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+
+#define MAX 1000001
+
+using namespace std;
+long long k;
+bool p[MAX];
+
+void isPrime()
+{
+	for (long long i = 2; i * i < MAX; ++i)
+	{
+		if (p[i])
+		{
+			for (long long j = i * i; j < MAX; j += i)
+				p[j] = false;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> k;
+
+	memset(p, true, MAX);
+
+	isPrime();
+
+	long long idx = 2;
+	long long cnt = 0;
+
+	while (1)
+	{
+		if (p[idx] == true)
+			cnt++;
+
+		if (cnt == k)
+			break;
+		idx++;
+	}
+
+	cout << idx;
+	return 0;
+}
