f823a1a11fce8d6f32d2db780adccbfc0f27d670
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-05-19 20:43:17 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int a,b;
+    cin >> a >> b;
+    
+    cout << a * b;
+
+	
+	return 0;
+}
