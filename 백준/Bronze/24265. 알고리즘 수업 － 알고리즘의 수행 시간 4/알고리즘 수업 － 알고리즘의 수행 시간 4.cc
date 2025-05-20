9edda6a1b79708f5086e9562425d4280cb498a2d
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-05-20 09:56:27 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <unordered_map>
+
+using namespace std;
+
+long long answer = 0;
+
+void MenOfPassion(int n) {
+
+	for (int i = 1; i < n; ++i)
+	{
+		for (int j = i+1; j <= n; ++j)
+			answer++;
+	}
+
+	return ; // 코드1
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	MenOfPassion(n);
+
+	cout << answer << '\n' << 2;
+
+	return 0;
+}
