bd7c0230b3ff46c9a84b214d8a171af3129bfe33
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-07-20 14:38:38 +0900

    [Silver III] Title: 등수 매기기, Time: 76 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <unordered_map>
+#include <bitset>
+#include <tuple>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	long long d = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		d += abs(v[i - 1] - i);
+	}
+
+	cout << d;
+	return 0;
+}
