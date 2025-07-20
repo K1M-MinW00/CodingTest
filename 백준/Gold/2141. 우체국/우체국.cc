2a3e62158a5fe33adeca9d84936c31da4eb4eab6
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-07-20 15:42:00 +0900

    [Gold IV] Title: 우체국, Time: 32 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.cc
+++ 백준/Gold/2141. 우체국/우체국.cc
@@ -1,30 +1,49 @@
 #include <iostream>
-#include <climits>
 #include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <unordered_map>
+#include <bitset>
+#include <tuple>
 using namespace std;
 
 int n;
-pair<long long, long long> arr[100001];
-long long sum;
+pair<long long, long long> v[100001];
+long long total = 0;
 
-int main() {
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
 	cin >> n;
 
-	for (int i = 1; i <= n; i++) {
-		cin >> arr[i].first >> arr[i].second;
-		sum += arr[i].second;
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> v[i].first >> v[i].second;
+		total += v[i].second;
 	}
 
-	sort(arr + 1, arr + 1 + n);
+	sort(v + 1, v + n + 1);
 
-	long long ans = 0;
+	long long temp = 0;
 
-	for (int i = 1; i <= n; i++) {
-		ans += arr[i].second;
-		if (ans >= (sum + 1) / 2) {
-			cout << arr[i].first;
+	for (int i = 1; i <= n; ++i)
+	{
+		temp += v[i].second;
+		if (temp >= (total + 1) / 2)
+		{
+			cout << v[i].first;
 			break;
 		}
 	}
+
+	return 0;
 }
