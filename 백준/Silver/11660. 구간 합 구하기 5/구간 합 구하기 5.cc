f0654dd4c5eba828abb17b246aad228c67caefef
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-10-26 11:55:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 108 ms, Memory: 10308 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,57 +1,46 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <unordered_map>
+#include <bitset>
+#include <tuple>
 using namespace std;
+#define N 1030
 
-int a[1024][1024] = { 0, };
-
+int v[N][N];
+int s[N][N];
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	cin.tie(0); cout.tie(0);
 
 	int n, m;
 	cin >> n >> m;
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> a[i][0];
 
-		for (int j = 1; j < n; ++j)
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= n; ++j)
 		{
-			int t;
-			cin >> t;
-			a[i][j] = a[i][j - 1] + t;
+			cin >> v[i][j];
+
+			s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + v[i][j];
 		}
 	}
 
 	int x1, y1, x2, y2;
-
 	for (int i = 0; i < m; ++i)
 	{
 		cin >> x1 >> y1 >> x2 >> y2;
 
-		int sum = 0;
-
-		if (x1 == x2 && y1 == y2)
-		{
-			sum = a[x1 - 1][y2 - 1] - a[x1 - 1][y2 - 2];
-			cout << sum << '\n';
-			continue;
-		}
-
-		for (int j = x1; j <= x2; ++j)
-		{
-			if (y1 == 1)
-			{
-				sum += a[j - 1][y2 - 1];
-			}
-			else
-			{
-				sum += a[j - 1][y2 - 1] - a[j - 1][y1 - 2];
-			}
-		}
-		cout << sum << '\n';
+		cout << s[x2][y2] - s[x2][y1 - 1] - s[x1 - 1][y2] + s[x1 - 1][y1 - 1] << '\n';
 	}
-
 	return 0;
 }
