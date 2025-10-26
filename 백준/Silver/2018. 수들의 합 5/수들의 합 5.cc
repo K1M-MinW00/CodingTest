906738810ed242ac401f6e882c88b8e69ccb9b05
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-10-26 17:29:25 +0900

    [Silver V] Title: 수들의 합 5, Time: 16 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -1,36 +1,56 @@
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <unordered_map>
+#include <bitset>
+#include <tuple>
 using namespace std;
+#define N 1000005
+#define M 1005
+
+long long v[N], s[N], c[M];
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(0); cout.tie(0);
 
 	int n;
 	cin >> n;
 
-	int s = 1, e = 1;
-
+	int l = 1, r = 1;
+	int sum = 1;
 	int answer = 0;
 
-	int sum = 0;
-
-	while (s <= e && e <= n)
+	while (l <= r)
 	{
 		if (sum < n)
-			sum += e++;
-
+		{
+			r++;
+			sum += r;
+		}
 		else if (sum > n)
-			sum -= s++;
-
+		{
+			sum -= l;
+			l++;
+		}
 		else
 		{
 			answer++;
-			sum -= s++;
+			r++;
+			sum += r;
 		}
 	}
 
-	cout << answer + 1;
+	cout << answer;
+
 	return 0;
 }
