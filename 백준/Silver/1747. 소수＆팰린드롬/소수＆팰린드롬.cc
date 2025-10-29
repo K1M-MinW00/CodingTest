cf3c4ec057f92b7f654872aa53cd4704de9502d6
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-10-29 22:49:38 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 48 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
@@ -1,48 +1,68 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <cmath>
 #include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <unordered_map>
+#include <bitset>
+#include <tuple>
+#include <sstream>
 using namespace std;
 
-bool isPrime(int num)
-{
-	if (num < 2)
-		return false;
+#define N 10000001
 
-	for (int i = 2; i <= sqrt(num); i++)
+int n;
+bool v[N];
+bool isPalindrome(int n)
+{
+	string str = to_string(n);
+
+	int l = str.length();
+
+	for (int i = 0; i <= l / 2; ++i)
 	{
-		if (num % i == 0)
+		if (str[i] != str[l - 1 - i])
 			return false;
 	}
-
 	return true;
 }
-
-bool isPalindrome(string str)
+void sieve()
 {
-	string rev;
+	v[1] = false;
 
-	rev = str;
-	reverse(rev.begin(), rev.end());
+	for (int i = 2; i < N; ++i)
+		v[i] = true;
 
-	if (str == rev)
-		return true;
+	for (int i = 2; i * i <= N; ++i)
+	{
+		if (v[i] == 0)
+			continue;
 
-	return false;
+		for (int j = i + i; j < N; j += i)
+		{
+			v[j] = false;
+		}
+	}
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int n;
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
 	cin >> n;
 
-	for (int i = n; ; i++)
+	sieve();
+
+	for (int i = n; i < N; ++i)
 	{
-		if (isPrime(i) && isPalindrome(to_string(i)))
+		if (v[i] != 0 && isPalindrome(i))
 		{
 			cout << i;
 			break;
