a15abb21af9f1ededf542517375dea72eccf4517
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-11-04 12:41:10 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 100 ms, Memory: 6092 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.cc
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.cc
@@ -1,68 +1,84 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <unordered_map>
+#include <bitset>
+#include <tuple>
 
 using namespace std;
 
-typedef pair<int, int> edge;
+int n, m, k;
+vector<vector<pair<int, int>>> v;
+priority_queue<int> d[1001];
 
-static int N,M,K;
-static int A[1001][1001];
-static priority_queue<int> D[1001];
+void dijkstra()
+{
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({ 0,1 });
+	d[1].push(0);
+
+	while (pq.size())
+	{
+		int cur_d = pq.top().first;
+		int cur = pq.top().second;
+
+		pq.pop();
+
+		for (int i = 0; i < v[cur].size(); ++i)
+		{
+			int next = v[cur][i].first;
+			int weight = v[cur][i].second;
+
+			if (d[next].size() < k)
+			{
+				d[next].push(cur_d + weight);
+				pq.push({ cur_d + weight,next });
+			}
+			else if (d[next].top() > cur_d + weight)
+			{
+				d[next].pop();
+				d[next].push(cur_d + weight);
+				pq.push({ cur_d + weight, next });
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (d[i].size() == k)
+		{
+			cout << d[i].top() << '\n';
+		}
+		else
+			cout << -1 << '\n';
+	}
+}
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
-    cin >> N >> M >> K;
- 
-    for (int i = 0; i < M; ++i)
-    {
-        int u, v, w;
-        cin >> u >> v >> w;
-        A[u][v] = w;
-    }
+	cin >> n >> m >> k;
 
-    priority_queue<edge, vector<edge>, greater<edge>> pq; // K 번째로 최단거리를 찾으므로 내림차순, Max 값보다 작다면 pop 하고 새로 추가
+	v.resize(n + 1);
 
+	for (int i = 0; i < m; ++i)
+	{
+		int a, b, w;
+		cin >> a >> b >> w;
+		v[a].push_back({ b,w });
+	}
 
-    pq.push({0, 1}); // 거리 , 정점
-    D[1].push(0);
+	dijkstra();
 
-    while (!pq.empty())
-    {
-        edge now = pq.top();
-        pq.pop();
-
-        for (int adj = 1; adj <= N; ++adj)
-        {
-            // 연결된 모든 노드에 대하여 검색하기
-            if (A[now.second][adj] != 0)
-            {
-                // 저장된 경로가 K개가 안 될 경우 그냥 추가
-                if (D[adj].size() < K)
-                {
-                    D[adj].push(now.first + A[now.second][adj]);
-                    pq.push({now.first + A[now.second][adj], adj}); // 거리 , 정점
-                }
-                else if (D[adj].top() > now.first + A[now.second][adj]) // 저장된 경로가 K개, 현재 가장 큰 값보다 작을 때만 추가
-                {
-                    D[adj].pop(); // 기존 큐에서 Max 값 먼저 삭제
-                    D[adj].push(now.first + A[now.second][adj]);
-                    pq.push({now.first + A[now.second][adj], adj});
-                }
-            }
-        }
-    }
-
-    for (int i = 1; i <= N; ++i)
-    {
-        if (D[i].size() == K)
-            cout << D[i].top() << '\n';
-        else
-            cout << -1 << '\n';
-    }
-
-    return 0;
+	return 0;
 }
