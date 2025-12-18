58d326d6363fe22c119fcfc1484444c2442bfe69
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2025-12-18 12:35:14 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 8 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -1,63 +1,45 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
 #include <string>
-#include <cstring>
-#include <cmath>
-#include <climits>
-#include <unordered_map>
-#include <bitset>
+#include <algorithm>
+#include <map>
+#include <stack>
 
 using namespace std;
-#define N 1005
 
 int n, m;
-int v[N][N];
-
-void solve()
-{
-	int l = 0;
-
-	for (int i = 1; i <= n; ++i)
-	{
-		for (int j = 1; j <= m; ++j)
-		{
-			if (v[i][j] != 0)
-			{
-				v[i][j] = min(v[i - 1][j - 1], min(v[i][j - 1], v[i - 1][j])) + 1;
-
-				if (l < v[i][j])
-					l = v[i][j];
-			}
-		}
-	}
-
-	cout << l * l << '\n';
-}
+int v[1001][1001];
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+	cin.tie(0);
+	cout.tie(0);
 
 	cin >> n >> m;
 
-	string str;
-	for (int i = 1; i <= n; ++i)
+	long long answer = 0;
+
+	for (int i = 0; i < n; ++i)
 	{
+		string str;
 		cin >> str;
 
 		for (int j = 0; j < m; ++j)
 		{
-			v[i][j + 1] = str[j] - '0';
+			v[i][j] = str[j] - '0';
+
+			if (v[i][j] == 1 && i > 0 && j > 0)
+			{
+				v[i][j] = min(v[i - 1][j - 1], min(v[i - 1][j], v[i][j - 1])) + v[i][j];
+			}
+
+			if (answer < v[i][j])
+				answer = v[i][j];
 		}
 	}
 
-	solve();
+	cout << answer * answer;
 
 	return 0;
 }
